e5ad55933d0801d0b160486024874950a3858762
Lin Zhou <xeontz@gmail.com>
2021-06-03 19:08:11 +0800

    update github action script

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-sudo apt-get -y install wget unzip
+sudo apt-get -y install curl unzip docker-ce docker-ce-cli
 
-wget -q https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -O /tmp/v2ray/v2ray.zip
-unzip /tmp/v2ray/v2ray.zip -d .
+curl -OJL https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip v2ray-linux-64.zip
+version=$(./v2ray --version | head -n1 | awk '{print $2}')
 
-docker build . -t pansila/v2ray_heroku:latest
-docker login
+docker build . -t pansila/v2ray_heroku:$version
+docker tag pansila/v2ray_heroku:$version pansila/v2ray_heroku:latest
+docker login -u pansila -p $DOCKER_PASSWORD
+docker push pansila/v2ray_heroku:$version
 docker push pansila/v2ray_heroku:latest
