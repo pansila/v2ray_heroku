47129c8d4b22fb69c5c90009333d570345a8d7f4
Lix Zhou <xeontz@gmail.com>
2021-06-11 15:25:35 +0800

    feat: enable DoH by heroku env var
    [skip ci]

--- run_v2ray.sh
+++ run_v2ray.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# $PORT is 443 by default
 sed -i -e "s/\"__PORT\"/$PORT/" -e "s/\"__UUID\"/\"$UUID\"/" /usr/local/etc/v2ray/config.json
+
+if [ "$DoH" = "true" ]; then
+    sed -i 's/^\(\s*\)\/\//\1/' /usr/local/etc/v2ray/config.json
+fi
 
 /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
